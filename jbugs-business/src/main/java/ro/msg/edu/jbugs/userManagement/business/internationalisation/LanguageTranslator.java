745d0349006092680c2bd39918f2f3f30a04dac7
lettherebeidiots <daiana_diac@yahoo.com>
2018-08-20 16:54:07 +0300

    inainte de merge

---
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/userManagement/business/internationalisation/LanguageTranslator.java
@@ -0,0 +1,13 @@
+package ro.msg.edu.jbugs.userManagement.business.internationalisation;
+
+import javax.ws.rs.GET;
+
+public class LanguageTranslator {
+
+	@GET
+	public int getFlag(){
+		return 0;
+	}
+
+
+}
