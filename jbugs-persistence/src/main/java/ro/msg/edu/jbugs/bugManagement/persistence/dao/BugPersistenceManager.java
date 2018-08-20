f821c4da501b4b4a4c78e11ac3f4df8d74f68263
DenisaTodorut <todorutdenisa21@gmail.com>
2018-08-20 16:03:02 +0200

    export 1

--- jbugs-persistence/src/main/java/ro/msg/edu/jbugs/bugManagement/persistence/dao/BugPersistenceManager.java
+++ jbugs-persistence/src/main/java/ro/msg/edu/jbugs/bugManagement/persistence/dao/BugPersistenceManager.java
@@ -5,9 +5,12 @@ import ro.msg.edu.jbugs.userManagement.persistence.entity.Role;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Optional;
 
 
 @Stateless
@@ -26,6 +29,17 @@ public class BugPersistenceManager {
     public List<Bug> getAllBugs() {
         TypedQuery<Bug> q = em.createNamedQuery(Bug.GET_ALL_BUGS,Bug.class);
         return q.getResultList();
+    }
+
+    public Optional<Bug> getBugByTitle(@NotNull String title) {
+        TypedQuery<Bug> q = em.createNamedQuery(Bug.GET_BUG_BY_TITLE,Bug.class)
+                .setParameter("title",title);
+        try {
+            return Optional.of(q.getSingleResult());
+        } catch (NoResultException ex) {
+            return Optional.empty();
+        }
+
     }
 
 }
