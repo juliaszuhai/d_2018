f346406aeda4a50b382b45f295a1ed8556414a60
biancatusa <bianca.tusa@yahoo.com>
2018-08-21 11:20:43 +0300

    bug-pt-toti

--- jbugs-client/src/main/clientApp/src/app/app.module.ts
+++ jbugs-client/src/main/clientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule, MatCheckboxModule, MatMenuModule, MatToolbarModule} fro
 import {AuthenticationModule} from './authentication/authentication.module';
 import { ListBugsComponent } from './bugs/list-bugs/list-bugs.component';
 import {BugsModule} from "./bugs/bugs.module";
+import {TranslatorModule} from "./translator/translator.module";
 
 
 const appRoutes: Routes = [
@@ -50,7 +51,8 @@ const appRoutes: Routes = [
     MatMenuModule,
     MatToolbarModule,
     AuthenticationModule,
-    BugsModule
+    BugsModule,
+    TranslatorModule
   ],
   providers: [],
   bootstrap: [AppComponent],
