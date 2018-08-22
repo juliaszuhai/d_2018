8ff2702b12e831c4a319676c4b8b35f2eb3c2c5d
lettherebeidiots <daiana_diac@yahoo.com>
2018-08-22 11:51:04 +0300

    trying to restore stuff

--- jbugs-client/src/main/clientApp/src/app/translator/translator.service.ts
+++ jbugs-client/src/main/clientApp/src/app/translator/translator.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "../../../node_modules/@angular/common/http";
-import {Locale} from "moment";
 import {Router} from "@angular/router";
-import {Http} from "@angular/http";
-import {map} from "rxjs/operators";
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
+
+
 export class TranslatorService {
   people:String="";
   baseURL = 'http://localhost:8080/jbugs/rest/language/';
