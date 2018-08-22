26f491f38d6bdb0847fdae116fa1d379562352a4
daniftinca <iftincadanalexandru@yahoo.ro>
2018-08-22 14:06:52 +0300

    added revoke roles from users added revoke permissions from roles + refactoring around

--- jbugs-business/src/main/java/ro/msg/edu/jbugs/userManagement/business/control/PermissionManagementController.java
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/userManagement/business/control/PermissionManagementController.java
@@ -34,6 +34,7 @@ public class PermissionManagementController {
     /**
      * Adds a role to a user. If the role type does not exist it will automatically add the new role type to the database.
      * If the user  already has the role, this method will do nothing.
+     *
      * @param roleType
      * @param username
      */
@@ -47,11 +48,68 @@ public class PermissionManagementController {
             User user = userOptional.get();
             normalizeUserRoles(user);
             if (!user.getRoles().contains(role)) {
-                userOptional.get().getRoles().add(role);
+                user.getRoles().add(role);
             }
         }
 
     }
+
+
+    /**
+     * Adds a permission to a role. If the permission type or role type don't exist it will automatically add them to the database.
+     * If the role already has the permission this method will do nothing.
+     *
+     * @param permissionType
+     * @param roleType
+     */
+    public void addPermissionToRole(String permissionType, String roleType) {
+
+        Role role = addRoleIfNotExists(roleType);
+        Permission permission = addPermissionIfNotExists(permissionType);
+        normalizesRolePermissions(role);
+        if (!role.getPermissions().contains(permission)) {
+            role.getPermissions().add(permission);
+        }
+    }
+
+    /**
+     * Revokes (deletes) a role from a user. If the user doesn't have the role to be revoked, the method will do nothing.
+     * If the specified roleType doesn't exist, nothing happens.
+     * @param roleType
+     * @param username
+     */
+    public void revokeRoleFromUser(String roleType, String username) throws BusinessException {
+
+        Optional<User> userOptional = userPersistenceManager.getUserByUsername(username);
+        Optional<Role> roleOptional = permissionPersistenceManager.getRoleByType(roleType);
+        if (roleOptional.isPresent()) {
+            if (!userOptional.isPresent()) {
+                throw new BusinessException(ExceptionCode.USERNAME_NOT_VALID);
+            } else {
+                User user = userOptional.get();
+                normalizeUserRoles(user);
+                user.getRoles().remove(roleOptional.get());
+            }
+        }
+    }
+
+
+    /**
+     * Revokes (deletes) a permission from a role. If the role doesn't have the permission to be revoked, the method will do nothing.
+     * If the specified permissionType doesn't exist, nothing happens.
+     * @param permissionType
+     * @param roleType
+     */
+    public void revokePermissionFromRole(String roleType, String permissionType) {
+        Optional<Role> roleOptional = permissionPersistenceManager.getRoleByType(roleType);
+        Optional<Permission> permissionOptional = permissionPersistenceManager.getPermissionByType(permissionType);
+
+        if(permissionOptional.isPresent() && roleOptional.isPresent()){
+            normalizesRolePermissions(roleOptional.get());
+            roleOptional.get().getPermissions().remove(permissionOptional.get());
+        }
+    }
+
 
     private void normalizeUserRoles(User user) {
         if (user.getRoles() == null) {
@@ -90,25 +148,5 @@ public class PermissionManagementController {
             permission = permissionOptional.get();
         }
         return permission;
-    }
-
-    /**
-     * Adds a permission to a role. If the permission type or role type don't exist it will automatically add them to the database.
-     * If the role already has the permission this method will do nothing.
-     * @param permissionType
-     * @param roleType
-     */
-    public void addPermissionToRole(String permissionType, String roleType) {
-
-        Role role = addRoleIfNotExists(roleType);
-        Permission permission = addPermissionIfNotExists(permissionType);
-        normalizesRolePermissions(role);
-        if (!role.getPermissions().contains(permission)) {
-            role.getPermissions().add(permission);
-        }
-    }
-
-    public void revokeRoleFromUser(String roleType, String username) {
-
     }
 }
