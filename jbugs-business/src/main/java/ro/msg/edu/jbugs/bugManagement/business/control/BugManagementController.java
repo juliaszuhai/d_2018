1215126f992e55bb34178d97cb1fb2de132c8c21
juliaszuhai <juliaszuhai@gmail.com>
2018-08-22 14:00:20 +0300

    Fixed conflicts

--- jbugs-business/src/main/java/ro/msg/edu/jbugs/bugManagement/business/control/BugManagementController.java
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/bugManagement/business/control/BugManagementController.java
@@ -148,7 +148,6 @@ public class BugManagementController implements BugManagement {
         return true;
     }
 
-
     @Override
     public BugDTO getBugById(Long id) throws BusinessException {
         Optional<Bug> bug=bugPersistenceManager.getBugById(id);
