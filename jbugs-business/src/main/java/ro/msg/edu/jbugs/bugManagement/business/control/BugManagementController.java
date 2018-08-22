6850336d38884565101b036ee5f07aa1731f1bac
DenisaTodorut <todorutdenisa21@gmail.com>
2018-08-22 09:52:59 +0200

    bugBusinesswithoutError

--- jbugs-business/src/main/java/ro/msg/edu/jbugs/bugManagement/business/control/BugManagementController.java
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/bugManagement/business/control/BugManagementController.java
@@ -160,6 +160,10 @@ public class BugManagementController implements BugManagement {
 
     }
 
+    @Override
+    public List<BugDTO> getBugsWithTitle(List<String> titles) {
+        return null;
+    }
 
 
 
