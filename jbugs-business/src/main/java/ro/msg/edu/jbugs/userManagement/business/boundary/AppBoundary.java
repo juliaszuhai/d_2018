4913d8d1b7e851a61ee9337853da253f94a0b347
daniftinca <iftincadanalexandru@yahoo.ro>
2018-08-23 13:14:54 +0300

    Refactoring endpoints

--- jbugs-business/src/main/java/ro/msg/edu/jbugs/userManagement/business/boundary/AppBoundary.java
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/userManagement/business/boundary/AppBoundary.java
@@ -1,16 +1,15 @@
 package ro.msg.edu.jbugs.userManagement.business.boundary;
 
 
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import ro.msg.edu.jbugs.bugManagement.business.boundary.*;
 
 import ro.msg.edu.jbugs.bugManagement.business.boundary.ViewBug;
 import ro.msg.edu.jbugs.internationalization.business.boundary.Language;
 
+import javax.servlet.Registration;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 @ApplicationPath("rest/")
@@ -18,8 +17,8 @@ public class AppBoundary extends Application {
 
     public Set<Class<?>> getClasses(){
         final Set<Class<?>> classes = new HashSet<>();
-        classes.add(JacksonJaxbJsonProvider.class);
         classes.add(Authentication.class);
+        classes.add(Registration.class);
         classes.add(ViewBug.class);
         classes.add(AddBug.class);
         classes.add(GenerateExcel.class);
