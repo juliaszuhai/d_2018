b6d9fb729d1174e224e396ca5eba5970646397b5
lettherebeidiots <daiana_diac@yahoo.com>
2018-08-25 17:48:30 +0300

    changed Language class, added dependency

--- jbugs-business/src/main/java/ro/msg/edu/jbugs/internationalization/business/boundary/Language.java
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/internationalization/business/boundary/Language.java
@@ -1,6 +1,6 @@
 package ro.msg.edu.jbugs.internationalization.business.boundary;
 
-
+import org.apache.commons.io.IOUtils;
 import ro.msg.edu.jbugs.internationalization.business.control.LanguageTranslator;
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
@@ -8,15 +8,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import java.io.File;
+
 
 @Path("/language")
 public class Language {
 
     @EJB
     private LanguageTranslator languageTranslator;
-
-    private static final String FILE_PATH = "C:\\Users\\iftind\\Desktop\\JBUGS_D\\d_2018\\jbugs-business\\src\\main\\java\\ro\\msg\\edu\\jbugs\\internationalization\\business\\utils\\Language_";
 
     /**
      * Retrieve JSON File specific to each language available
@@ -29,21 +27,16 @@ public class Language {
     @Produces("application/json")
     public Response getTranslation(@PathParam("id") int id) {
 
-        String language = languageTranslator.getLanguageSelected(id);
-        File file = null;
-
-
-
         try{
-            file = new File(FILE_PATH + language);
-            Response.ResponseBuilder response = Response.ok(file);
+
+            Response.ResponseBuilder response = Response.ok(
+                    IOUtils.toString(getClass().getClassLoader().getResourceAsStream(
+                            "Language_" + languageTranslator.getLanguageSelected(id) + ".json")));
+
             return response.build();
         }catch (Exception e){
-            return Response.status(Response.Status.BAD_REQUEST).build();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-
-
     }
-
 }
